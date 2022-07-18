7db90b48f4bb5e865a399b8e058fe6335ec48160
Donald Oakes <donald@oakesville.io>
2022-07-17 19:32:25 -0600

    custom

--- modules/tv/canned_searches.conf.php
+++ modules/tv/canned_searches.conf.php
@@ -11,47 +11,13 @@
 
 $Canned_Searches = array(
 
-    t('New Titles, Premieres')                              // See proglist.cpp
-        => 'program.manualid = 0'
-         . ' AND ('
-         . '   (program.previouslyshown = 0'
-         . '     AND (program.category = "Special"'
-         . '       OR program.programid LIKE "EP%0001"'
-         . '       OR (program.category_type = "series"'
-         . '         AND program.subtitle = "Pilot"))'
-         . '     AND DAYOFYEAR(program.originalairdate) ='
-         . '         DAYOFYEAR(program.starttime))'
-         . '   OR (program.category_type="movie"'
-         . '     AND program.stars > 0.5'
-         . '     AND program.airdate >= YEAR(NOW()) - 3)'
-         . ' )',
+     t('Horror Movies of the Thirties and Forties')
+         => 'category_type="movie"
+              AND (program.category="Horror" or program.category="Suspense" or program.category="Mystery")'
+           .' AND (program.airdate like "193%" or program.airdate like "194%")',
 
-    t('Movies')
-        => 'category_type="movie"',
-
-    t('Movies, 3&frac12; Stars or more')
-        => 'category_type="movie" AND program.stars > 0.8',
-
-    t('Movies, Stinkers (2 Stars or less)')
-        => 'category_type="movie" AND program.stars < 0.5625'
-          .' AND program.stars > 0.0',
-
-    t('Children\'s Movies')
-        => 'category="children" AND category_type="movie"',
-
-    t('Non-Series HDTV')
-        => 'hdtv & 1 AND category_type != "series"',
-
-    t('All HDTV')
-        => 'hdtv & 1',
-
-    t('Non-Music Specials')
-        => 'showtype="special" AND program.category NOT LIKE "music%"',
-
-    t('Music Specials')
-        => 'showtype="special" AND program.category LIKE "music%"',
-
-    t('Science Fiction Movies')
-        => 'category_type="movie" AND program.category="science fiction"',
+     t('Pre-Code Movies')
+         => 'category_type="movie"'
+           .' AND program.airdate < "1935"',
 
     );
